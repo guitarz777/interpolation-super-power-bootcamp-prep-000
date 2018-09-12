0ce49a91c2c9844a277e3515354ec3990f5eaca4
guitarz777@gmail.com <guitarz777@gmail.com>
2018-09-12 01:45:46 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
 # Write your #display_rainbow method here
 def display_rainbow(array)
-  puts "#{array[0].slice(0).upcase}: #{array[0]}, "
+  puts "#{array[0].slice(0).upcase}: #{array[0]}, array[1].slice(0).upcase}: #{array[0]} "
